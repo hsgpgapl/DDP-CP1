d03afea067d6ee9d162f51925054ec66b6c00bdb
Mark Friedman <hsgpgapl@gmail.com>
2015-08-06 11:21:10 -0400

    submission version

--- server.R
+++ server.R
@@ -6,30 +6,15 @@ rating <- function(factors){
     n <- length(factors)
 
 if (n == 0) {"you need help"} else
-    if (n == 1) {"mild"}          else
-        if (n == 2) {"crazy"}         else
-            if (n == 3) {"meshuggenah"}   else
-                if (n == 4) {"normal"}
+    if (n == 1) {"you are mildly aware"} else
+        if (n == 2) {"you are crazily aware"} else
+            if (n == 3) {"you are meshuggenah"} else
+                if (n == 4) {"you seem normal to me"}
 }
 
-tags <- function(factors){
-
-    t <- C() # initialize
-
-    if ('star'    %in% factors) {t <- c(t,"Trekkie!")}
-    if ('button'  %in% factors) {t <- c(t,"Your Tardis is here.")}
-    if ('rhps'    %in% factors) {t <- c(t,"Meatloaf for Dinner?")}
-    if ('rhps'    %in% factors) {t <- c(t,"Meatloaf for Dinner?")}
-    if ('podcast' %in% factors) {t <- c(t,"Black Jack Justice!")}
-
-    if (length(t) == 0) {t <- "None selected"}
-
-    return(t)
-}
 
 shinyServer(
     function(input, output) {
-        #output$ofactors   <- renderprint({tags(input$factors)})
         output$orating    <- renderPrint({rating(input$factors)})
     }
 )
--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 library(shiny)
+
 shinyUI(pageWithSidebar(
-    headerPanel("Model input"),
+    headerPanel("Culture Awareness Test"),
     sidebarPanel(
         checkboxGroupInput("factors", "Check all that apply",
                            c("Have you watched Star Trek?" = "star",
@@ -8,22 +9,22 @@ shinyUI(pageWithSidebar(
                              "Have you been to a 'Rocky Horror Picture Show' screening?" = "rhps",
                              "Is Decoder Ring Theatre your favorite Podcast?" = "podcast"
                              ))#,
-        #submitButton('Submit')
     ),
     mainPanel(
-            h2('Results of prediction'),
-            h4('Your Information'),
-            #h5('Factors selected'),
-            #verbatimTextOutput("ofactors"),
-            h4('Which means...'),
-            verbatimTextOutput("orating"),
-            br(),
+        h2("Documentation"),
+        p("1. This normalcy predictor uses 4 specific cultural indicators."),
+        p("2. If no indicators are check (the default condition), you may need cultural help."),
+        p("3. Check an indicator if you your response is 'yes' to the item."),
+        p("4. Your score is based on the number of indicators.  Please consult the Google machine to determine what the indicators mean."),
 
-            br(),
-            h2("Documentation"),
-            p("* This normalcy predictor uses 4 specific cultural indicators."),
-            p("* If no indicators are check (the default condition), you may need cultural help."),
-            p("* Your score is based on the number of indicators.  Please consult the Google machine to determine what the indicators mean.")
-           )
+        br(),
+        br(),
+
+        h2('Results of prediction'),
+        h4('Your selection of indicators means...'),
+        #h5('Factors selected'),
+        #verbatimTextOutput("ofactors"),
+        #h4('Which means...'),
+        verbatimTextOutput("orating")
+        )
 ))
-
